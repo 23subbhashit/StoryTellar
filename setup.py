f858e33c5748319c61c75d3bd667e5d8ac5e7043
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2021-04-12 20:43:30 +0530

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="storyscience",
-    version="1.3.0",
+    version="1.3.1",
     author="subbhashit mukherjee",
     author_email="subbhashitmukherjee@gmail.com",
     packages=["storyscience"],
@@ -23,7 +23,7 @@ setuptools.setup(
         'plotly>=4.9.0',
         'scikit-learn>=0.23.2',
         'tabulate>=0.8.9',
-        're>=2.2.1',
+        'regex>=2.2.1',
         'nltk>=3.4.5'
     ]
 )
